0a49cffcc305c5e4c7f1da7e4792005d39c65f7b
gshark <grigory.96@gmail.com>
2018-05-30 01:54:06 +0300

    hw9 done

--- src/Language.ml
+++ src/Language.ml
@@ -29,8 +29,11 @@ module Value =
     let of_string s = String s
     let of_array  a = Array  a
 
+    let rec my_init i n f = if i < n then (f i)::(my_init (i + 1) n f) else []
+    let list_init n f = my_init 0 n f
+
     let update_string s i x = String.init (String.length s) (fun j -> if j = i then x else s.[j])
-    let update_array  a i x = List.init   (List.length a)   (fun j -> if j = i then x else List.nth a j)
+    let update_array  a i x = list_init   (List.length a)   (fun j -> if j = i then x else List.nth a j)
 
   end
        
@@ -113,7 +116,25 @@ module Expr =
 
     (* The type of configuration: a state, an input stream, an output stream, an optional value *)
     type config = State.t * int list * int list * Value.t option
-                                                            
+           
+
+    let bool_to_int boolean = if boolean then 1 else 0
+
+    let calculate_binop op lval rval = match op with
+      | "+" -> lval + rval
+      | "-" -> lval - rval
+      | "*" -> lval * rval
+      | "/" -> lval / rval
+      | "%" -> lval mod rval
+      | "<" -> bool_to_int(lval < rval)
+      | ">" -> bool_to_int(lval > rval)
+      | "<=" -> bool_to_int(lval <= rval)
+      | ">=" -> bool_to_int(lval >= rval)
+      | "==" -> bool_to_int(lval == rval)
+      | "!=" -> bool_to_int(lval != rval)
+      | "&&" -> bool_to_int((lval != 0) && (rval != 0))
+      | "!!" -> bool_to_int((lval != 0) || (rval != 0))
+      | _ -> failwith ("Unsupported binary operation '" ^ op ^ "'")                                                 
     (* Expression evaluator
 
           val eval : env -> config -> t -> int * config
@@ -127,7 +148,38 @@ module Expr =
        which takes an environment (of the same type), a name of the function, a list of actual parameters and a configuration, 
        an returns a pair: the return value for the call and the resulting configuration
     *)                                                       
-    let rec eval env ((st, i, o, r) as conf) expr = failwith "Not implemented"
+    let rec eval env ((s, i, o, r) as conf) exp = match exp with
+    | Const c -> (s, i, o, Some (Value.of_int c))
+    | Var name -> (s, i, o, Some (State.eval s name))
+    | Binop (op, l, r) -> 
+      let ((_, _, _, Some lval) as conf') = eval env conf l in
+      let (s', i', o', Some rval) = eval env conf' r in 
+      let res = calculate_binop op (Value.to_int lval) (Value.to_int rval) in
+      (s', i', o', Some (Value.of_int res))
+    | Call (name, args) -> 
+      let (_, _, _, evaluated_args) = eval_list env conf args in
+      (env#definition env name evaluated_args conf)
+    | Array a -> 
+      let (s', i', o', res) = eval_list env conf a in
+      (s', i', o', Some (Value.of_array res))
+    | String str -> (s, i, o, Some (Value.of_string str))
+    | Elem (e, id) ->
+      let (s', i', o', Some evaluated_id) = eval env conf id in
+      let (s'', i'', o'', Some obj) = eval env (s', i', o', None) e in
+      let n = Value.to_int evaluated_id in
+      let res = (match obj with
+        | Value.Array arr -> List.nth arr n
+        | Value.String str -> let chr = String.get str n in 
+          Value.of_int (Char.code chr)) in
+      (s'', i'', o'', Some res)
+    | Length e ->
+      let (s', i', o', Some obj) = eval env conf e in
+      let res = (match obj with
+        | Value.Array arr -> List.length arr
+        | Value.String str -> String.length str) in
+      (s', i', o', Some (Value.of_int res))
+    | _ -> failwith "Unsupported pattern"
+
     and eval_list env conf xs =
       let vs, (st, i, o, _) =
         List.fold_left
@@ -145,8 +197,42 @@ module Expr =
          IDENT   --- a non-empty identifier a-zA-Z[a-zA-Z0-9_]* as a string
          DECIMAL --- a decimal constant [0-9]+ as a string                                                                                                                  
     *)
-    ostap (                                      
-      parse: empty {failwith "Not implemented"}
+    let make_op op = (ostap ($(op)), fun x y -> Binop(op, x, y))
+    
+    ostap (
+      parse: expr;
+
+      expr:
+        !(Ostap.Util.expr                              
+           (fun x -> x)                          
+           [|
+            `Lefta , [make_op "!!"];
+            `Lefta , [make_op "&&"];
+            `Nona ,  [make_op "<="; make_op ">="; make_op "<"; make_op ">"; make_op "=="; make_op "!="];                           
+            `Lefta , [make_op "+"; make_op "-"];
+            `Lefta, [make_op "*"; make_op "/"; make_op "%"]; 
+           |]                        
+           secondary                   
+         );
+      
+      secondary:
+        e: primary 
+        f:( "[" id:parse "]" {`Elem id}
+          | "." "length" {`Length})*
+        {List.fold_left (fun e f -> match f with
+          | `Elem id -> Elem (e, id)
+          | `Length -> Length e
+          ) e f};
+      
+      primary: 
+        n:DECIMAL {Const n}      
+      | c:CHAR {Const (Char.code c)}
+      | s:STRING {String (String.sub s 1 ((String.length s) - 2))}
+      | "[" arr:!(Util.list0 parse) "]" {Array arr}  
+      | name:IDENT "(" args:!(Util.list0)[parse] ")" {Call (name, args)}
+      | x:IDENT {Var x} 
+      | -"(" expr -")"
+      
     )
     
   end
@@ -186,11 +272,70 @@ module Stmt =
       in
       State.update x (match is with [] -> v | _ -> update (State.eval st x) v is) st
           
-    let rec eval env ((st, i, o, r) as conf) k stmt = failwith "Not implemented"
+    let merge_k st2 k = match k with
+    | Skip -> st2
+    | k' -> Seq(st2, k') 
+
+    let rec eval env ((s, i, o, r) as conf) k st = match st with
+        | Assign (x, id, e) -> 
+          let (s', i', o', Some evaluated_e) = Expr.eval env conf e in 
+          let (s'', i'', o'', evaluated_id) = Expr.eval_list env (s', i', o', None) id in
+          eval env (update s'' x evaluated_e evaluated_id, i'', o'', None) Skip k
+        | Seq (st1, st2) -> 
+          let k' = merge_k st2 k in
+          eval env conf k' st1
+        | Skip -> (match k with
+          | Skip -> (s, i, o, None)
+          | _ -> eval env (s, i, o, r) Skip k)
+        | If (cond, then_, else_) -> 
+          let (s', i', o', Some evaluated_cond) = Expr.eval env conf cond in
+          eval env (s', i', o', None) k (if (Value.to_int evaluated_cond) <> 0 then then_ else else_)
+        | While (cond, do_) -> 
+          let (s', i', o', Some evaluated_cond) = Expr.eval env conf cond in
+          if ((Value.to_int evaluated_cond) = 0) 
+          then 
+            eval env (s', i', o', None) Skip k 
+          else let k' = merge_k st k in
+            eval env (s', i', o', None) k' do_
+        | Repeat (do_, cond) -> 
+          let cond' = Expr.Binop ("==", cond, Expr.Const 0) in 
+          let while_ = While (cond', do_) in 
+          let k' = merge_k while_ k in
+          eval env (s, i, o, None) k' do_
+        | Call (name, args) -> 
+          let conf' = Expr.eval env conf (Expr.Call (name, args)) in
+          eval env conf' Skip k
+        | Return x -> (match x with
+          | None -> (s, i, o, None)
+          | Some x' -> Expr.eval env conf x')
+        | _ -> failwith "Unsupported pattern"
          
     (* Statement parser *)
+    let rec parse_else elif_ else_ = match elif_ with
+    | [] -> (match else_ with
+        | None -> Skip
+        | Some text -> text)
+    | (cond, then_)::tail -> If (cond, then_, parse_else tail else_)
+    
+    let pack args = match args with
+    | Some l -> l
+    | None -> []
+
     ostap (
-      parse: empty {failwith "Not implemented"}
+      parse: 
+          st1:stmt ";" st2:parse {Seq (st1, st2)}
+        | stmt; 
+
+      stmt: 
+          %"skip" {Skip}
+        | %"while" cond:!(Expr.parse) %"do" do_:parse %"od" {While (cond, do_)}
+        | %"repeat" do_:parse %"until" cond:!(Expr.parse) {Repeat (do_, cond)}
+        | %"for" init:parse "," cond:!(Expr.parse) "," inc:parse %"do" do_:parse %"od" {Seq (init, While (cond, Seq (do_, inc)))}
+        | %"if" cond:!(Expr.parse) %"then" then_:parse elif_:(%"elif" !(Expr.parse) %"then" parse)* else_:(%"else" parse)? %"fi" 
+            {If (cond, then_, parse_else elif_ else_)} 
+        | %"return" x:(!(Expr.parse))? {Return x}
+        | x:IDENT id:(-"[" !(Expr.parse) -"]")* ":=" e:!(Expr.parse) {Assign (x, id, e)}      
+        | name:IDENT "(" args:(!(Util.list)[ostap (!(Expr.parse))])? ")" {Call (name, pack args)}
     )
       
   end
--- src/SM.ml
+++ src/SM.ml
@@ -39,7 +39,45 @@ let split n l =
   in
   unzip ([], l) n
         
-let rec eval env ((cstack, stack, ((st, i, o) as c)) as conf) prg = failwith "Not implemented"
+let rec eval env ((cstack, stack, ((s, i, o) as c)) as conf) p = match p with
+| [] -> conf
+| cur::next -> (match cur with
+    | BINOP op -> (match stack with
+        | y::x::tail -> 
+            let res = Value.of_int (Expr.calculate_binop op (Value.to_int x) (Value.to_int y)) in 
+            eval env (cstack, res::tail, (s, i, o)) next 
+        | _ -> failwith "Stack size less than 2")
+    | CONST c -> eval env (cstack, (Value.of_int c)::stack, (s, i, o)) next
+    | STRING str -> eval env (cstack, (Value.of_string str)::stack, (s, i, o)) next
+    | LD x -> let v = State.eval s x in eval env (cstack, v::stack, (s, i, o)) next
+    | ST x -> (match stack with
+        | v::tail -> eval env (cstack, tail, (State.update x v s, i, o)) next
+        | _ -> failwith "Empty stack")
+    | STA (x, n) -> let v::tail, stack' = split (n + 1) stack in (match stack with
+        | v::tail -> eval env (cstack, stack', (Stmt.update s x v (List.rev tail), i, o)) next
+        | _ -> failwith "Empty stack")
+    | LABEL l -> eval env (cstack, stack, (s, i, o)) next
+    | JMP label -> eval env (cstack, stack, (s, i, o)) (env#labeled label)
+    | CJMP (cond, label) -> (match stack with
+        | (Value.Int x)::tail -> eval env (cstack, tail, (s, i, o)) (
+            let cond' = if cond = "nz" then x <> 0 else x = 0
+            in if cond' then (env#labeled label) else next)
+        | _ -> failwith "Empty stack")
+    | CALL (f, n, args) -> 
+        if env#is_label f then 
+          let cstack' = (next, s)::cstack in 
+          eval env (cstack', stack, (s, i, o)) (env#labeled f)
+        else
+          eval env (env#builtin conf f n args) next
+    | BEGIN (_, args, locals) -> 
+        let init_state = State.enter s (args@locals) in
+        let state_updater = fun x (s, v::tail) -> (State.update x v s, tail) in
+        let (s', stack') = List.fold_right state_updater args (init_state, stack) in
+        eval env (cstack, stack', (s', i, o)) next
+    | END | RET _ -> (match cstack with
+        | [] -> conf
+        | (next', s')::cstack' ->  eval env (cstack', stack, (State.leave s s', i, o)) next')
+    | _ -> failwith "Unsupported pattern")
 
 (* Top-level evaluation
 
@@ -74,6 +112,66 @@ let run p i =
   in
   o
 
+class env = 
+    object (self)
+        val mutable cnt = 0
+        method new_label = cnt <- cnt + 1; Printf.sprintf "generated_label_%d" cnt
+    end
+
+let rec expr_compile exp = match exp with
+| Expr.Var name -> [LD name]
+| Expr.Const x -> [CONST x]
+| Expr.String s -> [STRING s]
+| Expr.Array a -> 
+  let compiled_a = List.map expr_compile a in
+  List.concat compiled_a @ [CALL ("$array", List.length a, false)]
+| Expr.Binop (op, x, y) -> expr_compile x @ expr_compile y @ [BINOP op]
+| Expr.Call (name, args) ->  
+    let compiled_args = List.concat (List.map expr_compile args) in 
+    compiled_args @ [CALL (name, List.length args, false)]
+| Expr.Elem (a, id) -> let compiled_a = expr_compile a in
+  let compiled_id = expr_compile id in
+  compiled_a @ compiled_id @ [CALL ("$elem", 2, false)]
+| Expr.Length a -> let compiled_a = expr_compile a in
+  compiled_a @ [CALL ("$length", 1, false)]  
+| _ -> failwith "Unsupported pattern"
+
+let rec stmt_compile env st = match st with
+| Stmt.Assign (x, id, e) -> (match id with 
+  | [] -> expr_compile e @ [ST x]
+  | idl -> let compiled_id = List.concat (List.map expr_compile idl) in
+          compiled_id @ expr_compile e @ [STA (x, List.length idl)])
+| Stmt.Seq (s1, s2) -> stmt_compile env s1 @ stmt_compile env s2
+| Stmt.Skip -> []
+| Stmt.If (cond, then_, else_) -> 
+    let cond' = expr_compile cond in
+    let label_else = env#new_label in
+    let label_fi = env#new_label in
+    let compiled_then = stmt_compile env then_ in
+    let compiled_else = stmt_compile env else_ in
+    cond' @ [CJMP ("z", label_else)] @ compiled_then @ [JMP label_fi] @ [LABEL label_else] @ compiled_else @ [LABEL label_fi]
+| Stmt.While (cond, do_) -> 
+    let cond' = expr_compile cond in
+    let label_do = env#new_label in
+    let label_od = env#new_label in
+    let compiled_do = stmt_compile env do_ in
+    [LABEL label_do] @ cond' @ [CJMP ("z", label_od)] @ compiled_do @ [JMP label_do] @ [LABEL label_od]
+| Stmt.Repeat (do_, cond) ->
+    let cond' = expr_compile cond in
+    let label_repeat = env#new_label in
+    let compiled_do = stmt_compile env do_ in
+    [LABEL label_repeat] @ compiled_do @ cond' @ [CJMP ("z", label_repeat)]
+| Stmt.Call (name, args) ->
+    let compiled_args = List.concat (List.map expr_compile args) in 
+    compiled_args @ [CALL (name, List.length args, true)]
+| Stmt.Return x -> (match x with
+    | None -> [RET false]
+    | Some x' -> (expr_compile x') @ [RET true])
+| _ -> failwith "Unsupported pattern"
+
+let def_compile env (name, (args, locals, body)) = 
+    [LABEL name; BEGIN (name, args, locals)] @ (stmt_compile env body) @ [END]
+
 (* Stack machine compiler
 
      val compile : Language.t -> prg
@@ -81,4 +179,8 @@ let run p i =
    Takes a program in the source language and returns an equivalent program for the
    stack machine
 *)
-let compile (defs, p) = failwith "Not implemented"
+let compile (defs, p) = let env = new env in
+    let add_compiled ls def = ls @ (def_compile env def) in 
+    let compiled_defs = List.fold_left add_compiled [] defs in 
+    let main = stmt_compile env p in
+    main @ [END] @ compiled_defs
